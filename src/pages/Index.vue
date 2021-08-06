f802513922b45bfe848955e1923945334c69a4b8
Jack Woodrow <jackwoodrow99@gmail.com>
2021-08-05 22:55:41 -0400

    Configure Quasar to use apex charts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,44 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-card>
+
+      <q-card-section>
+        <div class="text-h6">ApexChart Example</div>
+      </q-card-section>
+
+      <q-separator inset />
+
+      <apexchart 
+      width="500" 
+      type="bar" 
+      :options="options" 
+      :series="series"
+      ></apexchart>
+
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+  import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'PageIndex'
-})
+  export default defineComponent({
+    name: 'PageIndex',
+    data(){
+      return { 
+        options: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }]
+      }
+    }
+  });
 </script>

44865b1b2a3b2685ab4a989cac700665fcb8edce
Jack Woodrow <jackwoodrow99@gmail.com>
2021-08-06 00:03:49 -0400

    Basic site layout

---
+++ src/pages/AllCryptos.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page>
+    <p>All Crypto</p>
+  </q-page>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'AllCrypto',
+    props: [],
+    data(){
+      return {
+
+      }
+    },
+    methods: {},
+    watch: {},
+    mounted(){
+
+    },
+    unmounted(){
+
+    }
+  });
+</script>
---
+++ src/pages/Analysis.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page>
+    <p>Analysis</p>
+  </q-page>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Analysis',
+    props: [],
+    data(){
+      return { 
+      }
+    },
+    methods: {},
+    watch: {},
+    mounted(){
+
+    },
+    unmounted(){
+
+    }
+  });
+</script>

ac2c495b911d9dce4d93414266c2d52090573f5e
Jack Woodrow <jackwoodrow99@gmail.com>
2021-10-23 15:27:53 -0400

    Add latest alt coin daily video to homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <q-page>
     <iframe src="https://cointelegraph.com/category/top-10-cryptocurrencies" title="Coin Telegraph: Top 10 Cryptocurrencies" width="500px" height="500px"></iframe>
-    <TradingAnalysisTool/>
+    <iframe width="560" height="315" :src="letestAltcoinDailyURI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <!-- <TradingAnalysisTool/> -->
   </q-page>
 </template>
 
@@ -12,18 +13,20 @@
   export default defineComponent({
     name: 'PageIndex',
     components: {
-      TradingAnalysisTool,
+      // TradingAnalysisTool,
     },
     props: [],
     data(){
       return {
+        letestAltcoinDailyURI: "",
       }
     },
     methods: {
     },
     watch: {},
     async mounted(){
-
+      let res = await this.$axios("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3DUCbLhGKVY-bJPcawebgtNfbw");
+      this.letestAltcoinDailyURI = `https://youtube.com/embed/${res.data.items[0].guid.slice(9, res.data.items[0].guid.length)}`;
     },
     unmounted(){
 
